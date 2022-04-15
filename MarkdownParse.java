ef698fed6709e7a30f1d8105251a9699970135a7
Badflar <badflar2000@gmail.com>
2022-04-15 11:35:12 -0700

    Update MarkdownParse.java

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -12,14 +12,28 @@ public class MarkdownParse {
         // find the next [, then find the ], then find the (, then read link upto next )
         int currentIndex = 0;
         while(currentIndex < markdown.length()) {
+            
             int openBracket = markdown.indexOf("[", currentIndex);
+            
             int closeBracket = markdown.indexOf("]", openBracket);
+            
             int openParen = markdown.indexOf("(", closeBracket);
+            
             int closeParen = markdown.indexOf(")", openParen);
+            
+            //System.out.println(markdown.substring(closeParen + 1));
+            if (closeParen + 2 <= markdown.length() - 1) {
+                if (!markdown.substring(closeParen + 1).equals("^[^[ ]*$") || markdown.indexOf("\n", openParen) != closeParen+1) {
+                    closeParen = markdown.indexOf(")", closeParen + 1);
+                }
+            }
+
+
             toReturn.add(markdown.substring(openParen + 1, closeParen));
             currentIndex = closeParen + 1;
         }
-
+        
+        
         return toReturn;
     }
 
